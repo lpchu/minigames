34d34e852888bc14eeb23a1645ab5f6aa1c70e79
lpchu <45775455+lpchu@users.noreply.github.com>
2019-01-15 14:40:47 -0500

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -1,8 +1,8 @@
 """ STOPWATCH mini game 
 Week 3 assignment of 'An introduction to interactive programming in Python' by Rice University (Coursera)
 
-Script can be run online @ http://www.codeskulptor.org/ using built-in simplegui module
-or offline using an equivalent SimpleGUICS2Pygame module (developed by Olivier Pirson)
+Script can be run online @ http://www.codeskulptor.org/ 
+using built-in simplegui module or offline using an equivalent SimpleGUICS2Pygame module
 
 The timer in offline mode could be slightly problematic (not sure why)
 """
